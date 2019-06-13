53f8cbe71c1fe1e29c696623b5f548e3a1b3a74c
joachimbauters <joachim.bauters@student.howest.be>
2019-06-14 00:25:02 +0200

    hiroku

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 require('dotenv').config();
 
 mongoose
@@ -17,11 +18,17 @@ mongoose
 
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 require('./app/routes/verhalen.routes.js')(app);
 require('./app/routes/qrcodes.routes.js')(app);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server luistert op poort ${process.env.PORT}`);
